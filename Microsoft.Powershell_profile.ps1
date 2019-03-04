6053d55fdfa320100ee10e05207face0f88abf13
LightOfSeven <LightOfSeven@users.noreply.github.com>
2019-03-04 10:44:10 +0000

    Update Microsoft.Powershell_profile.ps1
    Changed a significant amount: 
    Added Improved theme (removed backgrounds)
    Improved and replaced default ls to colourise the output - this is a modernised version of the previous function and now fully replaces the LS alias
    Changes the Powershell Tab Complete to a Bash style of the function - this can be toggled back by commenting / uncommenting the relevant lines

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -1,20 +1,17 @@
+# Set this to your internal Exchange URI or do not use the -online $false portion of the Connect-Exchange cmdlet
+$exchangeURI = "https://your.exchange.here/Powershell"
+
+# Bash style complete
+Set-PSReadlineKeyHandler -Key Tab -Function Complete
+# Standard Powershell complete
+# Set-PSReadlineKeyHandler -Key Tab -Function TabCompleteNext
+
 # Theme
 # Host Foreground
 $Host.PrivateData.ErrorForegroundColor = 'Red'
 $Host.PrivateData.WarningForegroundColor = 'Yellow'
 $Host.PrivateData.DebugForegroundColor = 'Green'
-$Host.PrivateData.VerboseForegroundColor = 'Blue'
 $Host.PrivateData.ProgressForegroundColor = 'Gray'
-
-# Host Background
-$Host.PrivateData.ErrorBackgroundColor = 'Gray'
-$Host.PrivateData.WarningBackgroundColor = 'Gray'
-$Host.PrivateData.DebugBackgroundColor = 'Gray'
-$Host.PrivateData.VerboseBackgroundColor = 'Gray'
-$Host.PrivateData.ProgressBackgroundColor = 'Cyan'
-
-# Further color edits
-$Host.ui.rawui.BackgroundColor = 'Black'
 
 # Check for PSReadline
 if (Get-Module -ListAvailable -Name "PSReadline") {
@@ -31,24 +28,7 @@ if (Get-Module -ListAvailable -Name "PSReadline") {
     $options.NumberForegroundColor = 'DarkGreen'
     $options.OperatorForegroundColor = 'DarkCyan'
     $options.ParameterForegroundColor = 'DarkCyan'
-    $options.StringForegroundColor = 'Blue'
-    $options.TypeForegroundColor = 'Blue'
     $options.VariableForegroundColor = 'Green'
-
-    # Background
-    $options.CommandBackgroundColor = 'Black'
-    $options.ContinuationPromptBackgroundColor = 'Black'
-    $options.DefaultTokenBackgroundColor = 'Black'
-    $options.EmphasisBackgroundColor = 'Black'
-    $options.ErrorBackgroundColor = 'Black'
-    $options.KeywordBackgroundColor = 'Black'
-    $options.MemberBackgroundColor = 'Black'
-    $options.NumberBackgroundColor = 'Black'
-    $options.OperatorBackgroundColor = 'Black'
-    $options.ParameterBackgroundColor = 'Black'
-    $options.StringBackgroundColor = 'Black'
-    $options.TypeBackgroundColor = 'Black'
-    $options.VariableBackgroundColor = 'Black'
 }
 
 # Coloured get-time 
@@ -66,11 +46,19 @@ function prompt
     return "> "
 }
 
-# LS.MSH 
-# Colorized LS function replacement 
-# /\/\o\/\/ 2006 
-# http://mow001.blogspot.com 
-function LL
+# Shows a line to make it easy to see command inputs
+$width = ($Host.UI.RawUI.WindowSize.Width - 2 - $(Get-Location).ToString().Length)
+$hr = New-Object System.String @('-',$width)
+Write-Host -ForegroundColor Red $(Get-Location) $hr
+
+# im for Import-Module
+Set-Alias im Import-Module
+
+# Delete ls alias so we can use it with some more 'flair' 
+del alias:ls -Force
+
+# Coloured LS
+function ls
 {
     param ($dir = ".", $all = $false) 
 
@@ -86,26 +74,51 @@ function LL
             ".cmd" {$host.ui.rawui.foregroundColor = "Red"} 
             ".msh" {$host.ui.rawui.foregroundColor = "Red"} 
             ".vbs" {$host.ui.rawui.foregroundColor = "Red"} 
+            ".ps1" {$host.ui.rawui.foregroundColor = "Red"} 
+            ".txt" {$host.ui.rawui.foregroundColor = "Cyan"}
+            ".json" {$host.ui.rawui.foregroundColor = "Cyan"}
+            ".csv" {$host.ui.rawui.foregroundColor = "Cyan"}
+            ".xml" {$host.ui.rawui.foregroundColor = "Cyan"}
+            ".log" {$host.ui.rawui.foregroundColor = "Cyan"}
+            ".conf" {$host.ui.rawui.foregroundColor = "Cyan"}
             Default {$host.ui.rawui.foregroundColor = $origFg} 
         } 
         if ($item.Mode.StartsWith("d")) {$host.ui.rawui.foregroundColor = "Green"}
+        if ($item.Mode.StartsWith("s")) {$host.ui.rawui.foregroundColor = "Gray"}
         $item 
     }  
     $host.ui.rawui.foregroundColor = $origFg 
 }
 
-function lla
+function lsa
 {
     param ( $dir=".")
-    ll $dir $true
+    ls $dir $true
 }
 
 function la { Get-ChildItem -force }
 
+# Change title of the prompt
+$host.ui.RawUI.WindowTitle = "Running as user $env:UserName"
 
-# Shows a line to make it easy to see command inputs
-$width = ($Host.UI.RawUI.WindowSize.Width - 2 - $(Get-Location).ToString().Length)
-$hr = New-Object System.String @('-',$width)
-Write-Host -ForegroundColor Red $(Get-Location) $hr
+# Import Exchange Online cmdlets
+Import-Module $((Get-ChildItem -Path $($env:LOCALAPPDATA+"\Apps\2.0\") -Filter Microsoft.Exchange.Management.ExoPowershellModule.dll -Recurse ).FullName|?{$_ -notmatch "_none_"}|select -First 1)
 
-Set-Alias im Import-Module
+function Connect-Exchange () {
+    [CmdletBinding()]
+    Param(
+        [bool]
+        $Online = $true
+    )
+    if($online){
+        $session = New-ExoPSSession
+        Import-PSSession $session
+    }
+    else{
+    $Creds = Get-Credential
+    $session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $exchangeURI -Authentication Kerberos -Credential $Creds
+    Import-PSSession $session -DisableNameChecking
+    }
+}
+
+cd c:\
