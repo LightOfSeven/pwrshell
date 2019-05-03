fed2d7ea3dda0942b3bceea8286b9b6f28cfd321
LightOfSeven <LightOfSeven@users.noreply.github.com>
2019-05-03 17:14:41 +0100

    Update Connect-EgnyteAPI.ps1
    Added a note about the bottom params.

--- Egnyte/Connect-EgnyteAPI.ps1
+++ Egnyte/Connect-EgnyteAPI.ps1
@@ -9,12 +9,14 @@ $params = @{
         password     = $Password
         grant_type   = 'password'
         scope        = 'Egnyte.user' # One of: https://developers.egnyte.com/docs/read/Public_API_Authentication#OAuth-Scopes
-        # Alternatively, scopes = "Egnyte.filesystem Egnyte.user Egnyte.group Egnyte.audit Egnyte.link Egnyte.permission Egnyte.salesforce Egnyte.bookmark Egnyte.launchwebsession"
+        # Alternatively you can include them all:
+        # scopes = "Egnyte.filesystem Egnyte.user Egnyte.group Egnyte.audit Egnyte.link Egnyte.permission Egnyte.salesforce Egnyte.bookmark Egnyte.launchwebsession"
     }
 }
 
 $AccessToken = (Invoke-RestMethod @params).access_token
 
+# After you get an access token, the rest here is just as a demo to pull user information of the connected user using the token
 $restparams = @{
     'uri'         = "$EgnyteDomain/pubapi/v1/userinfo"
     'method'      = 'Get'
