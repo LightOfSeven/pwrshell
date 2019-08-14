0468830cec4600700d150c84225537fe673d809b
LightOfSeven <LightOfSeven@users.noreply.github.com>
2019-08-14 17:41:40 +0100

    fix User path variable versus users name
    The users' names were not being pulled, the entire file object was. Fixed by adding an additional variable, $users

--- Teams_Uninstaller.ps1
+++ Teams_Uninstaller.ps1
@@ -5,31 +5,32 @@ Removes Teams including the Machine Installer that reinstalls each user login.
 
 $UsersRoot = [System.IO.Path]::Combine(($env:Public).trimend("\Public")) 
 $AllUsersPath = Get-ChildItem $UsersRoot
+$Users = $AllUsersPath.FullName
 # Stop the process if it's running
-Stop-Process -Name "Teams.exe" -Force -ErrorAction SilentlyContinue
+# Stop-Process -Name "Teams.exe" -Force -ErrorAction SilentlyContinue
 
 # Run the removal from AppData on all users
-ForEach($User in $AllUsersPath){
-    $TeamsPath = [System.IO.Path]::Combine($User, 'AppData', 'Local', 'Microsoft', 'Teams')
-    $TeamsUpdateExePath = [System.IO.Path]::Combine($User, 'AppData', 'Local', 'Microsoft', 'Teams', 'Update.exe')
+ForEach($User in $Users){
+    $TeamsPath = Convert-Path "$user\AppData\Local\Microsoft\Teams" -ErrorAction SilentlyContinue
+    $TeamsUpdateExePath = "$TeamsPath\Update.exe"
     try{
         if (Test-Path -Path $TeamsUpdateExePath) {
             Write-Output "Uninstalling Teams process"
             # Uninstall app
             $proc = Start-Process -FilePath $TeamsUpdateExePath -ArgumentList "-uninstall -s" -PassThru
             $proc.WaitForExit()
-            Write-Output "Teams process uninstalled for $user"
+            Write-Output "Success! Teams process uninstalled for $user.Name"
         }
         else {
-            Write-Output "Teams update exe in AppData is not present for $User"
+            Write-Output "Not Found. Teams update exe in AppData is not present for $User"
         }
         if (Test-Path -Path $TeamsPath) {
             Write-Output "Deleting Teams directory"
             Remove-Item -Path $TeamsPath -Recurse -Force
-            Write-Ouput "Teams directory deleted for $user"
+            Write-Ouput "Success! Teams directory deleted for $user"
         }
         else {
-            Write-Output "No teams install was found for $User at path $TeamsPath"
+            Write-Output "Not Found. Teams install was not found for $User at path $TeamsPath"
         }
     }
     catch{
@@ -40,10 +41,10 @@ try{
     if (Test-Path -Path $TeamsMachineInstallerPath) {
         Write-Output "Deleting Teams Machine Installer"
         Remove-Item -Path $TeamsMachineInstallerPath -Recurse -Force
-        Write-Output "Successful, Teams Machine Installer deleted on $Env:COMPUTERNAME"
+        Write-Output "Success! Teams Machine Installer deleted on $Env:COMPUTERNAME"
     }
     else {
-        "Not Teams Machine Installer found on $Env:COMPUTERNAME"
+        "Not Found. Not Teams Machine Installer found on $Env:COMPUTERNAME"
     }
 }
 catch{
