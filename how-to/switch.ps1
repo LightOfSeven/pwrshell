2aae30b7a3c9c3432f229d30a7d1f2735b94889b
LightOfSeven <LightOfSeven@users.noreply.github.com>
2019-09-05 11:02:25 +0100

    add initial commit

---
+++ how-to/switch.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS 
+- This example explains how to use a switch (aka case) instead of nested ifs / foreach loops
+.DESCRIPTION 
+- Switches save time, space, and are easier to read!
+#>
+
+Switch($example){
+    "foo"{}
+    "bar"{}
+    "foobar"{}
+}
