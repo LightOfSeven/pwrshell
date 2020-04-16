a57dc267bd2e499125d12b0fedeaa3385b995c1d
LightOfSeven <LightOfSeven@users.noreply.github.com>
2020-04-16 13:26:54 +0100

    fixes menu
    The menu output was reworked as it was static in the initial commit. This now returns the choice from the menu properly.

--- how-to/menu.ps1
+++ how-to/menu.ps1
@@ -5,6 +5,7 @@
 function Show-Menu {
     param (
         [string]$Title,
+        [Parameter(Mandatory=$True)]
         [array]$Choices
     )
     
@@ -12,6 +13,10 @@ function Show-Menu {
     Write-Host "================ $Title ================"
     
     $i = 0
+    $total = ($choices | Measure-Object).count
+
+    if($total -eq 1){return $Choices}
+
     foreach($Choice in $Choices){
         $i = $i+1
         Write-Host "$i : $Choice"
@@ -19,19 +24,14 @@ function Show-Menu {
     Write-Host "Q: Press 'Q' to quit."
 
     do{
-        $selection = Read-Host "Please make a selection"
-        switch ($selection)
-        {
-        '1' {
-        'You chose option #1'
-        } '2' {
-        'You chose option #2'
-        } '3' {
-          'You chose option #3'
+        do{
+          $selection = $null
+          $selection = Read-Host "Select object [Q to quit instead]"
+          if($selection -eq 'Q'){return $null}
         }
-        }
-        pause
-     }
-     until ($selection -ne $null)
+        while($selection -notmatch '^\d+$') #validate numeric only entry
+    }
+    while([int]$selection -lt 1 -or [int]$selection -gt $total) #validate numeric entry is in range
 
+    $Choices[$selection]
 }
